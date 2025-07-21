f24d85b59fbdc5b36b08be17238462c9776f75b6
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-07-21 20:22:02 +0000

    new file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 122456
+let accountEmail = "anshu@gmail.com"
+var accountPassword = "220099"
+let accountState;
+accountCity = "Lucknow"
+
+// accountID = "222222"
+accountEmail = "as@gmail.com"
+accountPassword = "3334455"
+
+accountCity = "Agra"
+
+console.table([accountID,accountEmail, accountPassword, accountCity, accountState])
+
+/* prefer not to use var
+because issue in block scope and functional scope*/
