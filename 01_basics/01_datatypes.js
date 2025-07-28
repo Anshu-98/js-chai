13835f6021f90b4ff8c40e1d3040f312f4b5a95d
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-07-28 19:47:40 +0000

    datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,3 +1,26 @@
 "use strict"            // treat all js code as newer version
 
-alert(3 +3);
+// alert(3 +3);         we are using nodejs, not the browser
+
+console.log( 3+3);
+
+let name = "Anshu"
+let age = 21
+let isLogged = true;
+let temp = null;
+
+// type of datatypes 
+// number => 2 to the power 53
+// bigint 
+// string = "yes"
+// boolean = true/false 
+// undefined => no value assinged
+// null => standalone value
+
+// object
+
+console.log(typeof null)         // object
+console.log(typeof undefined)    // undefined
+
+
+
