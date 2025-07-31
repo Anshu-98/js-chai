639fd3b0719510201a22c08609667ffd72dcf8bd
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-07-31 19:35:33 +0000

    type conversion completed

--- 01_basics/01_varConversion.js
+++ 01_basics/01_varConversion.js
@@ -1,10 +1,28 @@
-let score = NaN
+let score = "anshu"
 
 let isNumber = Number(score);
 
-console.log(typeof isNumber);
-console.log(isNumber);
+//console.log(typeof isNumber);
+//console.log(isNumber);
 
 //  33 => 33 
-// "anshu" =
+// "anshu" = NaN
+// true => 1, false = 0
+
+let isLoggedIn = 112
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 0 => false , 1 => true
+// "" => false
+// "anshu" => true
+
+let someString = 22
+
+let isString = String(someString)
+
+console.log(isString)
+console.log(typeof isString)
+
 
