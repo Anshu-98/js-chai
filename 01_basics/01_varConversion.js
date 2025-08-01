ad7d23edafe5d9ec21b2a951bd0a6c4382fed06b
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-08-01 19:38:39 +0000

    operations discussed

--- 01_basics/01_varConversion.js
+++ 01_basics/01_varConversion.js
@@ -12,7 +12,7 @@ let isNumber = Number(score);
 let isLoggedIn = 112
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 0 => false , 1 => true
 // "" => false
@@ -22,7 +22,43 @@ let someString = 22
 
 let isString = String(someString)
 
-console.log(isString)
-console.log(typeof isString)
+// console.log(isString)
+// console.log(typeof isString)
 
+// *************************************-Operations-*******************************************
+
+let value = 3
+let negValue = -value
+
+// console.log(value, negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "anshu"
+let str2 = " sharma"
+
+console.log(str1+ str2)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(+true)
+console.log(+"")
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
 
