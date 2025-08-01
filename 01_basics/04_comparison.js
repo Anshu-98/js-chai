8cb93d9b3941e564d535cf87d434704d6d6f4ce0
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-08-01 20:01:50 +0000

    comparison completed

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log("2"> 1)
+// console.log("00"> 1)
+
+// write strict code or either use TS to avoid these ambiguity in code
+
+
+// console.log(null> 0)
+// console.log(null== 0)
+// console.log(null>= 0)
+
+
+// console.log(undefined= 0)
+// console.log(undefined> 0)
+// console.log(undefined>= 0)
+
+// == first convert then checks the values
+// === didn't convert datatype and stricly checks the values
+
+console.log("2" === 2)
