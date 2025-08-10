124e56a4a25dec29891b65daedb9970f8f789234
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-08-10 06:20:43 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+let myName = "anshu"
+let repoNo = 5;
+
+// console.log(myName + " " + repoNo + "sharma");
+
+console.log(`Hello my name is ${myName.toUpperCase()} and my repo count ${repoNo}`);
+
+let gameName = new String('Contra');
+
+console.log(gameName)
