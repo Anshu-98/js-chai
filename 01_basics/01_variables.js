c24eacf90333c218b7fac994603b99c1ed3b6e70
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-15 06:52:48 +0000

    block and fuction scope definations are added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,7 +10,24 @@ accountPassword = "3334455"
 
 accountCity = "Agra"
 
-console.table([accountID,accountEmail, accountPassword, accountCity, accountState])
+// console.table([accountID,accountEmail, accountPassword, accountCity, accountState])
 
 /* prefer not to use var
 because issue in block scope and functional scope*/
+
+// understand block and function scope with the help of below example
+
+{
+    let x = 10
+    const y = 20
+    console.log(x,y);  // Acessible here 
+}
+// console.log(x,y);   Error: x and y are not defined
+
+function test() {
+    if (true) {
+        var z = 30;
+    }
+    console.log(z); // Accessible here, outside the function scope
+}
+test();
