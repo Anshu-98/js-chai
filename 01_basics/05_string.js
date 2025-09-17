fbe47ddbb21ec19c65186b21e62c46554fa8f21e
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-17 06:51:36 +0000

    .

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,10 +1,13 @@
-let myName = "anshu"
+let myName = "Anshu"
 let repoNo = 5;
 
-// console.log(myName + " " + repoNo + "sharma");
+// console.log(myName + " " + repoNo + "sharma"); //not recomended 
 
+//`` This is template literal- used for string interpolation
 console.log(`Hello my name is ${myName.toUpperCase()} and my repo count ${repoNo}`);
 
 let gameName = new String('Contra');
 
-console.log(gameName)
+// console.log(gameName)
+console.log(gameName.charAt([3]));
+console.log(gameName.toUpperCase())
