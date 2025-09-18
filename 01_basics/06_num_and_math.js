f4c21f8cd72a2e4b4f1f8853db0e2e954458fceb
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-18 19:50:49 +0000

    number and math topics are covered

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,38 @@
+const score = 400;
+
+const balance = new Number(100);
+
+// console.log(balance.toString())
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))  // use this is e-commerce app
+
+let sin = 5123.5654
+
+// console.log(sin.toPrecision(4));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))   // Indian standard
+
+// ---------------------Maths --------------------------
+
+// console.log(Math);
+
+// console.log(Math.abs(-5))
+// console.log(Math.pow(2,3))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(4,5,3,4,1))
+// console.log(Math.max(4,5,3,4,1))
+
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+ 10)  // formula
+
+
+
