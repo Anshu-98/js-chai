0ad82d608f762e7b761bf60819f42d915b384610
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-18 18:58:00 +0000

    --

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,27 +4,39 @@ let repoNo = 5;
 // console.log(myName + " " + repoNo + "sharma"); //not recomended 
 
 //`` This is template literal- used for string interpolation
-console.log(`Hello my name is ${myName.toUpperCase()} and my repo count ${repoNo}`);
+// console.log(`Hello my name is ${myName.toUpperCase()} and my repo count ${repoNo}`);
 
 let gameName = new String('Contra');
 
 // console.log(gameName)
-console.log(gameName.charAt([3]));
-console.log(gameName.toUpperCase());
+// console.log(gameName.charAt([3]));
+// console.log(gameName.toUpperCase());
 let newString = gameName.slice(-4,4);
 
-console.log(newString);
+// console.log(newString);
 
 let string2 = "     Anshu    ";
-console.log(string2);
-console.log(string2.trim());
+// console.log(string2);
+// console.log(string2.trim());
 
 let url = "https://anshu.com/anshu%20sharma";
 
-console.log(url.replace("%20", "-"));
+// console.log(url.replace("%20", "-"));
 
-console.log(url.includes('anshu'));
+// console.log(url.includes('anshu'));
 
-console.log(gameName.length);
+// console.log(gameName.length);
 
+
+const sentence = "Mario is a classic video game"
+
+let index = 6;
+
+let buzzWord = "specification";
+
+console.log(`The index of ${index} returns the character ${sentence.at(index)}`)
+
+console.log(sentence.concat(index))
+
+console.log(buzzWord.slice(-8,3))
 
