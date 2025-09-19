a4172663c8f663128467292405d76888facf61c5
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-19 19:15:26 +0000

    new file added

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -33,6 +33,3 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random()*(max-min +1))+ 10)  // formula
-
-
-
---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,38 @@
+// Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.getDate());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2025, 9, 19)
+// let myCreatedDate = new Date(2025, 9, 19, 1, 13, 7)
+
+let myCreatedDate = new Date("2025/09/13")
+
+// console.log(myCreatedDate.toLocaleString())
+
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime());  // both will geive output is milisecond
+
+// console.log(Math.floor(Date.now()/1000))  // converted to sececonds ** for comparison always use ms
+
+
+let newDate = new Date();
+
+let form = newDate.toLocaleString('default',{
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    month: "long"
+})
+
+console.log(form);
