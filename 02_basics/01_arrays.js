b1c558639292a48c791c6ff21254200bbd5f67aa
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-19 20:09:45 +0000

    --

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// -----------------Arrays-------------------
+// stores diferrent type of data
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ["IronMan", "Spider-Man", "Hulk"]
+
+const myArr2 = new Array(1,2,3,4)           // we can also declare array like this
+
+console.log(myArr[4])
+
+// ----------------Array Methods--------------
+
