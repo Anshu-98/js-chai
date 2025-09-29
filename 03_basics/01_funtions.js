0adb589dddc5015f0173731a069fad89043abe22
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-09-29 19:08:02 +0000

    functions

---
+++ 03_basics/01_funtions.js
@@ -0,0 +1,29 @@
+//-------------------Functions---------
+
+function myName(){                  // functions declration
+console.log("A")
+console.log("N")
+console.log("S")
+console.log("H")
+console.log("U")
+}
+
+// myName  -- function reference
+
+// myName()    -- fucntion calling/execution
+
+function addNumbers(number1, number2){
+
+    // let result = number1+number2
+    // return result
+    
+    return number1+number2
+
+}
+
+// addNumbers(3,4);    // 7
+
+const result = addNumbers(3,4)
+console.log(result)
+
+
