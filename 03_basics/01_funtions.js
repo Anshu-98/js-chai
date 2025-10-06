d7704eefcbd28cfe83a993891d8da967a0c93d10
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-10-06 19:36:04 +0000

    funtions completed

--- 03_basics/01_funtions.js
+++ 03_basics/01_funtions.js
@@ -28,7 +28,52 @@ function addNumbers(number1, number2){
 const result = addNumbers(3,4)
 // console.log("Result:" ,result)
 
+function isUserLogged(user){
+    // if (user=== undefined)
+        
+    if (!user)    {
 
+    console.log("please provide username")
+        return
+    }
+    
+    return `${user} is logged in`
+}
 
+isUserLogged("Anshu")
 
+// console.log(isUserLogged("Anshu"));
+// console.log(isUserLogged());  // undefined is logged in
 
+function calculateCartPrice(val1,val2, ...num1){        // "..." rest operator bundules the arguments/values. also take rest of the arguments
+
+    return num1
+
+}
+
+// console.log(calculateCartPrice(200,400,500,650,550))
+
+const user = {
+    username:"Anshu",
+    price: 299
+}
+
+function userDetails(anyobject)
+{
+    return console.log(`Username is ${anyobject.username} and price is ${anyobject.price}.`)
+}
+
+// userDetails(user)
+
+userDetails({
+    username: "Jake",
+    price: 399
+})
+
+const itemPrices = [200,300,500,700];
+
+function returnPrice(getArray){
+    return getArray[1]
+}
+
+// console.log(returnPrice(itemPrices))
