e761b72c905a26de9001d0554f1e97094ea82446
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-11-12 19:17:44 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,19 @@
+//if statement
+
+// let num1 = 3
+// if (num1 >10){
+
+//     console.log(`the number ${num1} is greater than to 10`)
+
+// }
+// else if( num1 === 10){
+//     console.log(`the number ${num1} is equal to 10`)
+// }
+// else{
+//     console.log(`the number ${num1} is less than to 10`)
+// }
+
+// shorthand notation
+// const balance = 1000
+// if (balance> 500) console.log("you have enough balance");  // this has implicit scope without {}
+
