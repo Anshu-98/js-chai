ca2ad97e644ae24acad88c83cba649524603d576
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-11-24 12:15:34 +0000

    for loop completed

---
+++ 05_iterations/one.js
@@ -0,0 +1,40 @@
+// for
+
+// for(let i= 0; i<= 10; i++){
+//     const element = i;
+//     if (element == 5){
+//     console.log("5 is best number")
+//     }
+//     console.log(element);
+// }
+
+
+// for(let i=2; i<= 5; i++){
+//     console.log(`table of ${i}`)
+//     for(let j=1; j<= 10; j++){
+//         // console.log(`Outer loop ${j} and inner loop ${i}`)
+//         console.log(`${i} X ${j} = ${i*j}`)
+//     }
+// }
+
+// let myArray = ["Flash", "Arrow", "Superman", "Batman"];
+
+// for(let i=0; i<myArray.length; i++){
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+for(let i=1; i<=10; i++){
+    if(i==5){
+        console.log("Detected 5");
+        // break;
+        continue
+    }
+    console.log(`Value of i is ${i}`);    
+}
+
+
+
