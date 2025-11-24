c594ab375f7c5a0000a4166c268d0ebb777693c9
Anshu-98 <84659548+Anshu-98@users.noreply.github.com>
2025-11-24 12:33:16 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0
+
+while (index < 5) {
+//   console.log(`index is: ${index}`)
+  index = index +2
+}
+
+let heros = ['spiderman', 'batman', 'superman', 'ironman']
+
+while (heros.length > 0) {
+  let hero = heros.pop()
+//   console.log(`hero is: ${hero}`)
+}
+
+
+let score = 6
+
+do {
+    console.log(`score is: ${score}`)
+    score++
+} while (score < 5)
